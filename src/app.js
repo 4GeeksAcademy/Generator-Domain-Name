82b0b63aea47ad51640be906b476702ce78c72f7
Marina Myssak <marina.myssak@outlook.com>
2023-12-12 05:01:08 +0600

    complite generator-domain-name

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./bootstrap.js";
+// import "./style.css";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let list = listOfDomainNames();
+  let addNames = document.createElement("ul");
+  list.forEach(names => {
+    const name = document.createElement("li");
+    name.textContent = names;
+    addNames.appendChild(name);
+  });
+  document.body.appendChild(addNames);
+  function listOfDomainNames() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+    let extension = [".com", ".edu", ".net", ".us"];
+    let string = [];
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < extension.length; l++) {
+            string.push(`${pronoun[i]}${adj[j]}${noun[k]}${extension[l]}`);
+          }
+        }
+      }
+    }
+    return string;
+  }
 };
+listOfDomainNames();
